37f93cf551c259ae5394b4a626ce4433d97bebec
Daniel Chagas <daniel.chagas@runtalent.it>
2021-07-12 22:18:21 -0300

    primeiro commit

---
+++ src/Core/SharedKernel/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharedKernel.Models
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Services/UsuariosExternosService/Configurations/SwaggerConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace UsuariosExternosService.Configurations
+{
+    public static class SwaggerConfigurations
+    {
+        public static IServiceCollection AddSwaggerConfigurations(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApp", Version = "v1" });
+            });
+
+            return services;
+        }
+
+        public static void UseSwaggerConfigurations(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp v1"));
+        }
+    }
+}
