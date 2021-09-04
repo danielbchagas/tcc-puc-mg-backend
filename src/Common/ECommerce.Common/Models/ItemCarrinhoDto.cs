c1aedab7789cf2bde7d98babe04f4f9ce24361f1
Daniel Chagas <daniel.chagas@runtalent.it>
2021-09-04 20:15:48 -0300

    refatorando

---
+++ src/Common/ECommerce.Common/Models/ItemCarrinhoDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ECommerce.Common.Models
+{
+    public class ItemCarrinhoDto
+    {
+        public ItemCarrinhoDto(Guid id, string nome, int quantidade, decimal valor, string imagem, Guid produtoId, Guid carrinhoId)
+        {
+            Id = id;
+            Nome = nome;
+            Quantidade = quantidade;
+            Valor = valor;
+            Imagem = imagem;
+            ProdutoId = produtoId;
+            CarrinhoId = carrinhoId;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+        public string Imagem { get; set; }
+
+        public Guid ProdutoId { get; set; }
+        public Guid CarrinhoId { get; set; }
+    }
+}
