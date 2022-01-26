d531074872d53ff04f3c910f71349aa3c9867799
Daniel B. Chagas <daniel.boasq@gmail.com>
2022-01-26 19:16:38 -0300

    identity
    seed, migration, database renamed

--- Services/Identity/ECommerce.Identity.Api/Data/ApplicationDbContext.cs
+++ Services/Identity/ECommerce.Identity.Api/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Identity.Api.Data
@@ -8,6 +9,47 @@ namespace ECommerce.Identity.Api.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            Seed(builder);
+
+            base.OnModelCreating(builder);
+        }
+
+        private void Seed(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Seeding a  'Administrator' role to AspNetRoles table
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7210", Name = "Administrator", NormalizedName = "ADMINISTRATOR" });
+
+
+            //a hasher to hash the password before seeding the user to the db
+            var hasher = new PasswordHasher<IdentityUser>();
+
+
+            //Seeding the User to AspNetUsers table
+            builder.Entity<IdentityUser>().HasData(
+                new IdentityUser
+                {
+                    Id = "8e445865-a24d-4543-a6c6-9443d048cdb9", // primary key
+                    UserName = "admin",
+                    NormalizedUserName = "ADMIN",
+                    PasswordHash = hasher.HashPassword(null, "Pa$$w0rd")
+                }
+            );
+
+
+            //Seeding the relation between our user and role to AspNetUserRoles table
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                    UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
+                }
+            );
         }
     }
 }
