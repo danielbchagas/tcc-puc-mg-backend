7ba5878e4c9d89f2347e3eae15c64c3265df7c29
Daniel <daniel.boasq@gmail.com>
2022-02-06 10:44:30 -0300

    core.apis
    options configuration

---
+++ Core/Apis/ECommerce.Core.Apis/Configurations/OptionsConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Core.Apis.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Core.Apis.Configurations
+{
+    public static class OptionsConfiguration
+    {
+        public static IServiceCollection AddOptionsConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<RabbitMq>(config => configuration.GetSection("RabbitMqOptions").Bind(config));
+            services.Configure<Jwt>(config => configuration.GetSection("JwtOptions").Bind(config));
+            services.Configure<Swagger>(config => configuration.GetSection("SwaggerOptions").Bind(config));
+
+            return services;
+        }
+    }
+}
---
+++ Core/Apis/ECommerce.Core.Apis/Options/RabbitMq.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Core.Apis.Options
+{
+    public class RabbitMq
+    {
+        public string MessageBus { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
