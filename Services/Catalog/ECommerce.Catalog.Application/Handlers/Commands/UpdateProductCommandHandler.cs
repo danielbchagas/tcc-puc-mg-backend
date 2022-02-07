8cc60f5cc2499aabdc77ef8148d6a86c7247fd47
Daniel <daniel.boasq@gmail.com>
2022-02-06 21:23:15 -0300

    fdxcgbgfdsdghsdfgfdzgfgds
    fgdsvbxfgsdhcxdtrfg

---
+++ Services/Catalog/ECommerce.Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Catalog.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Catalog.Application.Handlers.Commands
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, ValidationResult>
+    {
+
+    }
+}
