01bde03f0132b89356a0ad7cb9ed5f89b348bb85
Daniel B. Chagas <daniel.boasq@gmail.com>
2022-02-06 22:26:17 -0300

    product update handler

--- Services/Catalog/ECommerce.Catalog.Api/Configurations/DependencyInjectionConfiguration.cs
+++ Services/Catalog/ECommerce.Catalog.Api/Configurations/DependencyInjectionConfiguration.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using ECommerce.Catalog.Application.Commands;
+using ECommerce.Catalog.Application.Handlers.Commands;
 using ECommerce.Catalog.Application.Handlers.Queries;
 using ECommerce.Catalog.Application.Queries;
 using ECommerce.Catalog.Domain.Interfaces.Repositories;
 using ECommerce.Catalog.Domain.Models;
 using ECommerce.Catalog.Infrastructure.Repositories;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,11 +19,11 @@ namespace ECommerce.Catalog.Api.Configurations
         {
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             
-            
             services.AddMediatR(typeof(Startup));
             services.AddScoped<IRequestHandler<GetProductQuery, Product>, GetProductQueryHandler>();
             services.AddScoped<IRequestHandler<FilterProductsQuery, IEnumerable<Product>>, FilterProductsQueryHandler>();
             services.AddScoped<IRequestHandler<GetProductsQuery, IEnumerable<Product>>, GetProductsQueryHandler>();
+            services.AddScoped<IRequestHandler<UpdateProductCommand, ValidationResult>, UpdateProductCommandHandler>();
 
             services.AddScoped<IProductRepository, ProductRepository>();
         }
--- Services/Catalog/ECommerce.Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
+++ Services/Catalog/ECommerce.Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
@@ -1,20 +1,44 @@
 ﻿using ECommerce.Catalog.Application.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ECommerce.Catalog.Domain.Interfaces.Repositories;
+using FluentValidation.Results;
 using MediatR;
 using System.Threading;
-using FluentValidation.Results;
+using System.Threading.Tasks;
 
 namespace ECommerce.Catalog.Application.Handlers.Commands
 {
     public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, ValidationResult>
     {
-        public Task<ValidationResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        private readonly IProductRepository _repository;
+
+        public UpdateProductCommandHandler(IProductRepository repository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+        }
+
+        public async Task<ValidationResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var validation = new ValidationResult();
+
+            var product = await _repository.Get(request.Id);
+
+            product.Id = request.Id;
+            product.Name = request.Name;
+            product.Description = request.Description;
+            product.Enabled = request.Enabled;
+            product.Image = request.Image;
+            product.Value = request.Value;
+            product.Quantity = request.Quantity;
+
+            validation = product.Validate();
+
+            if (validation.IsValid)
+            {
+                await _repository.Update(product);
+                await _repository.UnitOfWork.Commit();
+            }
+
+            return await Task.FromResult(validation);
         }
     }
 }
