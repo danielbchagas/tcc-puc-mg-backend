a05975acd8e9fc0999c314bca28af25df99637c1
Daniel B. Chagas <daniel.boasq@gmail.com>
2022-02-06 22:10:55 -0300

    references fixed

--- Services/Catalog/ECommerce.Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
+++ Services/Catalog/ECommerce.Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
@@ -1,15 +1,20 @@
 ﻿using ECommerce.Catalog.Application.Commands;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using System.Threading;
+using FluentValidation.Results;
 
 namespace ECommerce.Catalog.Application.Handlers.Commands
 {
     public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, ValidationResult>
     {
-
+        public Task<ValidationResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
