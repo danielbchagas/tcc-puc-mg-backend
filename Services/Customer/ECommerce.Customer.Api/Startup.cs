d3265b866ac38dc45f8a5deadbce8c74dba4de57
Daniel B. Chagas <daniel.boasq@gmail.com>
2022-04-09 12:54:39 -0300

    grpc web

--- Services/Customer/ECommerce.Customer.Api/Startup.cs
+++ Services/Customer/ECommerce.Customer.Api/Startup.cs
@@ -67,6 +67,8 @@ namespace ECommerce.Customer.Api
 
             app.UseRouting();
 
+            app.UseGrpcWeb();
+
             app.UseAuthentication();
             app.UseAuthorization();
 
@@ -75,7 +77,7 @@ namespace ECommerce.Customer.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapGrpcService<CustomerGrpcService>();
+                endpoints.MapGrpcService<CustomerGrpcService>().EnableGrpcWeb();
             });
         }
     }
