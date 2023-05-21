83f920f1949899ba588e6a49c9a94328cd0a4f7d
Daniel Chagas <daniel.boasq@gmail.com>
2023-05-21 11:58:55 -0300

    gateway removido

--- Gateways/Ordering/ECommerce.Ordering.Gateway/DTOs/Response/PaymentResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ECommerce.Ordering.Gateway.DTOs.Response
-{
-    public class PaymentResponse
-    {
-        public decimal Value { get; set; }
-        public bool Approved { get; set; }
-        public string Message { get; set; }
-    }
-}
--- Gateways/Ordering/ECommerce.Ordering.Gateway/Interfaces/ICustomerGrpcClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ECommerce.Ordering.Gateway.Interfaces
-{
-    public interface ICustomerGrpcClient
-    {
-        Task<Customer.Api.Protos.GetUserResponse> GetCustomer(Customer.Api.Protos.GetUserRequest request);
-    }
-}
