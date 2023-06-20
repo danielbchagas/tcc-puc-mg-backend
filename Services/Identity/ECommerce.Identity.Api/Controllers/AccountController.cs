0171adc0bf2804715ceb9d2e45b6aaf13f50cbe7
Daniel Chagas <daniel.boasq@gmail.com>
2023-06-20 20:56:43 -0300

    refatoracao no endpoint de signup

--- Services/Identity/ECommerce.Identity.Api/Controllers/AccountController.cs
+++ Services/Identity/ECommerce.Identity.Api/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ECommerce.Customers.Api.Protos;
 using ECommerce.Identity.Api.Constants;
 using ECommerce.Identity.Api.Handler;
 using ECommerce.Identity.Api.Interfaces;
@@ -46,49 +47,44 @@ namespace ECommerce.Identity.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost("sign-up")]
-        public async Task<IActionResult> SignUp(SignUpUserRequest user)
+        public async Task<IActionResult> SignUp(SignUpUserRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState.Values.SelectMany(e => e.Errors.Select(e => e.ErrorMessage)));
 
-            var identityUser = new IdentityUser();
+            var user = new IdentityUser();
+
+            var identityResult = await _userManager.CreateAsync(
+                    new IdentityUser { UserName = request.Email, Email = request.Email, PhoneNumber = request.Phone },
+                    request.Password
+                );
+
+            if (!identityResult.Succeeded)
+                return BadRequest(identityResult.Errors.Select(e => e.Description));
+
+            user = await _userManager.FindByEmailAsync(request.Email);
+
+            identityResult = await _userManager.AddToRoleAsync(user, UserRoles.CUSTOMER);
+
+            if (!identityResult.Succeeded)
+                return BadRequest(identityResult.Errors.Select(e => e.Description));
 
             try
             {
-                var identityResult = await _userManager.CreateAsync(
-                    new IdentityUser { UserName = user.Email, Email = user.Email, PhoneNumber = user.Phone}, 
-                    user.Password
-                );
-
-                if (!identityResult.Succeeded)
-                {
-                    var errors = identityResult.Errors.Select(e => e.Description);
-
-                    _logger.LogError($"Erro em: {HttpContext.Request.Path}", errors);
-                    return BadRequest(errors);
-                }
-
-                identityUser = await _userManager.FindByEmailAsync(user.Email);
-
-                await _userManager.AddToRoleAsync(identityUser, UserRoles.CUSTOMER);
-
-                user.Id = Guid.Parse(identityUser.Id);
-
-                var customer = _mapper.Map<CustomerRequest>(user);
-                customer.Address = _mapper.Map<AddressRequest>(await _viaCepService.GetAddress(user.ZipCode));
+                var customer = await ComposeCustomer(user.Id, request);
 
                 await _customerRabbitMqClient.CreateCustomer(customer);
             }
             catch (Exception e)
             {
-                if(identityUser != null)
-                    await _userManager.DeleteAsync(identityUser);
+                if(user != null)
+                    await _userManager.DeleteAsync(user);
 
                 _logger.LogError(e.Message, e.InnerException);
                 return BadRequest(ResponseMessages.USER_NOT_CREATED);
             }
 
-            var token = await _jwtHandler.GenerateNewToken(user.Email);
+            var token = await _jwtHandler.GenerateNewToken(request.Email);
 
             return Ok(token);
         }
@@ -96,17 +92,17 @@ namespace ECommerce.Identity.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost("sign-in")]
-        public async Task<IActionResult> SignIn(SignInUserRequest user)
+        public async Task<IActionResult> SignIn(SignInUserRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState.Values.SelectMany(e => e.Errors.Select(e => e.ErrorMessage)));
 
-            var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, isPersistent: false, lockoutOnFailure: true);
+            var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, isPersistent: false, lockoutOnFailure: true);
 
             if (!result.Succeeded)
                 return BadRequest(ResponseMessages.LOGIN_FAILED);
 
-            var token = await _jwtHandler.GenerateNewToken(user.Email);
+            var token = await _jwtHandler.GenerateNewToken(request.Email);
 
             return Ok(token);
         }
@@ -131,5 +127,13 @@ namespace ECommerce.Identity.Api.Controllers
 
             return Ok(token);
         }
+
+        private async Task<CustomerRequest> ComposeCustomer(string id, SignUpUserRequest user)
+        {
+            var customer = _mapper.Map<CustomerRequest>(user);
+            customer.Id = Guid.Parse(id);
+            customer.Address = _mapper.Map<AddressRequest>(await _viaCepService.GetAddress(user.ZipCode));
+            return customer;
+        } 
     }
 }
