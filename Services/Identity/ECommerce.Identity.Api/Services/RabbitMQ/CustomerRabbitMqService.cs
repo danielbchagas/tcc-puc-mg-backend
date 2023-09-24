d4e9d591689127804c45be5e1513bcc6061e9a3d
Daniel Chagas <daniel.boasq@gmail.com>
2023-09-24 10:54:17 -0300

    retry pattern

--- Services/Identity/ECommerce.Identity.Api/Services/RabbitMQ/CustomerRabbitMqService.cs
+++ Services/Identity/ECommerce.Identity.Api/Services/RabbitMQ/CustomerRabbitMqService.cs
@@ -1,50 +1,66 @@
-﻿using ECommerce.Identity.Api.Interfaces;
+﻿using System;
+using ECommerce.Identity.Api.Interfaces;
 using ECommerce.Identity.Api.Models.Request;
 using ECommerce.Identity.Api.Options;
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace ECommerce.Identity.Api.Services.RabbitMQ
 {
     public class CustomerRabbitMqService : ICustomerRabbitMqClient
     {
-        public RabbitMqOption _rabbitMqOptions;
+        private readonly ILogger<CustomerRabbitMqService> _logger;
+        private readonly RabbitMqOption _rabbitMqOptions;
 
-        public CustomerRabbitMqService(IOptions<RabbitMqOption> options)
+        public CustomerRabbitMqService(ILogger<CustomerRabbitMqService> logger, IOptions<RabbitMqOption> options)
         {
+            _logger = logger;
             _rabbitMqOptions = options.Value;
         }
 
         public Task CreateCustomer(CustomerRequest request)
         {
-            var factory = new ConnectionFactory
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .RetryAsync(3, (exception, retryCount, context) =>
+                {
+                    _logger.LogError($"Retry counter: {retryCount}", JsonSerializer.Serialize(exception));
+                });
+            
+            return retryPolicy.ExecuteAsync(() =>
             {
-                HostName = _rabbitMqOptions.Host,
-                UserName = _rabbitMqOptions.Username,
-                Password = _rabbitMqOptions.Password
-            };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
+                var factory = new ConnectionFactory
+                {
+                    HostName = _rabbitMqOptions.Host,
+                    UserName = _rabbitMqOptions.Username,
+                    Password = _rabbitMqOptions.Password
+                };
 
-            channel.QueueDeclare(queue: _rabbitMqOptions.Queue,
-                                 durable: true,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+                using var connection = factory.CreateConnection();
+                using var channel = connection.CreateModel();
 
-            var body = JsonSerializer.SerializeToUtf8Bytes(request);
+                channel.QueueDeclare(queue: _rabbitMqOptions.Queue,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
 
-            var properties = channel.CreateBasicProperties();
-            properties.Persistent = true;
+                var body = JsonSerializer.SerializeToUtf8Bytes(request);
 
-            channel.BasicPublish(exchange: _rabbitMqOptions.Exchange,
-                                 routingKey: _rabbitMqOptions.Queue,
-                                 basicProperties: properties,
-                                 body: body);
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
 
-            return Task.CompletedTask;
+                channel.BasicPublish(exchange: _rabbitMqOptions.Exchange,
+                    routingKey: _rabbitMqOptions.Queue,
+                    basicProperties: properties,
+                    body: body);
+
+                return Task.FromResult(Task.CompletedTask);
+            });
         }
     }
 }
