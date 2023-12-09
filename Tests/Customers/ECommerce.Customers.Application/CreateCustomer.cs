2c53f29b2a9ef7cd09e12cb27a577dfdbc629fc3
Daniel Chagas <daniel.boasq@gmail.com>
2023-12-09 11:33:53 -0300

    mudanca no create customer test

--- Tests/Customers/ECommerce.Customers.Application/CreateCustomer.cs
+++ Tests/Customers/ECommerce.Customers.Application/CreateCustomer.cs
@@ -3,31 +3,32 @@ using ECommerce.Customers.Application.Commands.Customer;
 using ECommerce.Customers.Application.Handlers.Commands.Customer;
 using ECommerce.Customers.Domain.Interfaces.Data;
 using ECommerce.Customers.Domain.Interfaces.Repositories;
+using ECommerce.Customers.Domain.Models;
 using Moq;
 
 namespace ECommerce.Customers.Application
 {
     public class CreateCustomer
     {
-        private readonly CreateCustomerCommandHandler _handler;
+        private CreateCustomerCommandHandler _handler;
         private readonly Fixture _fixture = new Fixture();
 
-        public CreateCustomer()
+
+        [Fact]
+        public async Task Create_Customer_Should_Be_False()
         {
+            // Arrange
             var repo = new Mock<ICustomerRepository>();
             var uow = new Mock<IUnitOfWork>();
 
             _handler = new CreateCustomerCommandHandler(repo.Object, uow.Object);
-        }
 
-        [Fact]
-        public async Task Create_Customer_Should_Be_True()
-        {
-            // Arrange
             var document = _fixture.Build<CreateDocumentCommand>().Create();
             var email = _fixture.Build<CreateEmailCommand>().Create();
             var phone = _fixture.Build<CreatePhoneCommand>().Create();
             var address = _fixture.Build<CreateAddressCommand>().Create();
+
+            var customer = _fixture.Build<Customer>().Create();
 
             var command = new CreateCustomerCommand(Guid.NewGuid(), string.Empty, string.Empty, document, email, phone, address);
 
@@ -36,6 +37,8 @@ namespace ECommerce.Customers.Application
 
             // Assert
             Assert.False(response.Item1.IsValid);
+            repo.Verify(x => x.Create(customer), Times.Never);
+            uow.Verify(x => x.Commit(), Times.Never);
         }
     }
 }
